fdc7558161e3b43f34b7a67082e227e2b067b07c
HieuBui <buiminhhieu.hd@gmail.com>
2020-08-17 00:21:33 +0700

    Fix

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -11,7 +11,7 @@ class StaffTest extends TestCase
         $staff = new Staff;
 
         $staff->first_name = "HIeuBUi";
-        $staff->last_name = "Bao";
+        $staff->last_name = "Baosdfsfd";
 
         $this->assertEquals('Nguyen Bao', $staff->getFullName());
     }
